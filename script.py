961b23cddede592499caa57dfe6c2f93dd37bde1
marcin22 <blabla@somewhere.com>
2020-07-08 14:14:52 +0100

    Removed some code

--- script.py
+++ script.py
@@ -1,21 +1,4 @@
-import math
-import sys
-from os import rename
-
 import requests
-
-# print(sys.version)f
-# print(sys.executable)
-
-name = input("Your Name? ")
-print("Hello,", name)
-
-
-def greet(who):
-    test = "Test"
-    greeting = f"Hello {who}"
-    return greeting
-
 
 r = requests.get("https://coreyms.com")
 print(r.status_code)
